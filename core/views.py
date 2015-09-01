4f75a02089f1ac1eab2e65718037121355a7a87f
ap0ca1ip53 <jfacardoso@gmail.com>
2015-09-01 17:36:57 -0300

    retirando o restante dos dispatch

--- core/views.py
+++ core/views.py
@@ -58,9 +58,6 @@ class InstalacaoUpdate(LoginMixin, UpdateView, SuccessMessageMixin):
     success_url = '/instalacoes/'
     fields = ['sigla', 'descricao', 'orgao', 'cidade']
 
-    def dispatch(self, *args, **kwargs):
-        return super(InstalacaoUpdate, self).dispatch(*args, **kwargs)
-
     def get(self, request, *args, **kwargs):
         if Instalacao.objects.count() > 0:
             return super(InstalacaoUpdate, self).get(request, *args, **kwargs)
@@ -88,10 +85,7 @@ class EquipeCreate(LoginMixin, CreateView):
     success_url = '/equipes/'
     fields = ['equipe']
 
-    def dispatch(self, *args, **kwargs):
-        return super(EquipeCreate, self).dispatch(*args, **kwargs)
-
-
+    
 class EquipeUpdate(LoginMixin, UpdateView):
     model = Equipe
     success_url = '/equipes/'
@@ -118,26 +112,17 @@ class TipoDeEquipamentoList(ListView):
         else:
             return redirect(reverse('core:tipodeequipamento_add'))
 
-    def dispatch(self, *args, **kwargs):
-        return super(TipoDeEquipamentoList, self).dispatch(*args, **kwargs)
-
 
 class TipoDeEquipamentoCreate(LoginMixin, CreateView):
     model = TipoDeEquipamento
     success_url = '/tipodeequipamento/'
     fields = ['descricao', 'nivelTensao', 'imagem']
 
-    def dispatch(self, *args, **kwargs):
-        return super(TipoDeEquipamentoCreate, self).dispatch(*args, **kwargs)
-
 
 class TipoDeEquipamentoUpdate(LoginMixin, UpdateView):
     model = TipoDeEquipamento
     success_url = '/tipodeequipamento/'
     fields = ['descricao', 'nivelTensao', 'imagem']
-
-    def dispatch(self, *args, **kwargs):
-        return super(TipoDeEquipamentoUpdate, self).dispatch(*args, **kwargs)
 
 
 class TipoDeEquipamentoDelete(LoginMixin, DeleteView):
@@ -147,10 +132,6 @@ class TipoDeEquipamentoDelete(LoginMixin, DeleteView):
 
 class TipoDeEquipamentoDetail(LoginMixin, DetailView):
     model = TipoDeEquipamento
-
-    def dispatch(self, *args, **kwargs):
-        return super(TipoDeEquipamentoDetail, self).dispatch(*args, **kwargs)
-
 
     def get(self, request, *args, **kwargs):
         if TipoDeEquipamento.objects.count() > 0:
@@ -167,17 +148,11 @@ class EquipamentoList(ListView):
         else:
             return redirect(reverse('core:equipamento_add'))
 
-    def dispatch(self, *args, **kwargs):
-        return super(EquipamentoList, self).dispatch(*args, **kwargs)
-
 
 class EquipamentoCreate(LoginMixin, CreateView):
     model = Equipamento
     success_url = '/equipamento/'
     fields = ['instalacao', 'tipo_de_equipamento', 'posicao_operacional']
-
-    def dispatch(self, *args, **kwargs):
-        return super(EquipamentoCreate, self).dispatch(*args, **kwargs)
 
 
 class EquipamentoUpdate(LoginMixin, UpdateView):
@@ -185,16 +160,10 @@ class EquipamentoUpdate(LoginMixin, UpdateView):
     success_url = '/equipamento/'
     fields = ['instalacao', 'tipo_de_equipamento', 'posicao_operacional']
 
-    def dispatch(self, *args, **kwargs):
-        return super(EquipamentoUpdate, self).dispatch(*args, **kwargs)
-
 
 class EquipamentoDelete(LoginMixin, DeleteView):
     model = Equipamento
     success_url = '/equipamento/'
-
-    def dispatch(self, *args, **kwargs):
-        return super(EquipamentoDelete, self).dispatch(*args, **kwargs)
 
 
 class FuncionarioList(ListView):
@@ -206,26 +175,17 @@ class FuncionarioList(ListView):
         else:
             return redirect(reverse('core:funcionario_add'))
 
-    def dispatch(self, *args, **kwargs):
-        return super(FuncionarioList, self).dispatch(*args, **kwargs)
-
 
 class FuncionarioCreate(LoginMixin, CreateView):
     model = Funcionario
     success_url = '/funcionario/'
     fields = ['equipe', 'usuario', 'nome']
 
-    def dispatch(self, *args, **kwargs):
-        return super(FuncionarioCreate, self).dispatch(*args, **kwargs)
-
 
 class FuncionarioUpdate(LoginMixin, UpdateView):
     model = Funcionario
     success_url = '/funcionario/'
     fields = ['equipe', 'usuario', 'nome']
-
-    def dispatch(self, *args, **kwargs):
-        return super(FuncionarioUpdate, self).dispatch(*args, **kwargs)
 
 
 class FuncionarioDelete(LoginMixin, DeleteView):
